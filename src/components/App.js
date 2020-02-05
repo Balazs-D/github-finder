8c0a60496f36a9a433ae0085702b81872a4e09b4
Balazs-D <balazs.danyadi@gmail.com>
2020-02-05 17:16:27 +0100

    full application is refactored to context hook

--- src/components/App.js
+++ src/components/App.js
@@ -3,42 +3,19 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // Components
-import Search from './users/Search';
 import Alert from './Layouts/Alert';
 import Navbar from './Layouts/Navbar';
-import Users from './users/Users';
 import User from './users/User';
 import About from './pages/About';
 import GithubState from '../context/github/GithubState';
-import AlertState from '../context/alert/AlertState'
-
+import AlertState from '../context/alert/AlertState';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound'
 
 // Style
 import '../assets/css/App.css';
 
 const App = () => {
-  
-// const [alert, setAlert] = useState(null);
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP__GITHUB_CLIENT_SECRET}`
-  //   );
-
-  //   this.setState({ users: res.data, loading: false });
-  // }
-
-
-
-  // Get single user
-
-
-  // Get users repos
-
-
-
   return (
     <GithubState>
       <AlertState>
@@ -48,18 +25,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+        <h1>Not Found</h1>
+        <p>The site you are looking for doesn't exists.</p>
+            
+        </div>
+    )
+}
+
+export default NotFound; 
